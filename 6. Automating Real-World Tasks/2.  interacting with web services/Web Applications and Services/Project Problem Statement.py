0b5fa924362695c67064d77ad857c40e3fa89cf0
Jairo Watts <jwatts.pajaro@gmail.com>
2023-05-17 11:43:20 -0500

    Create Project Problem Statement.py

---
+++ 6. Automating Real-World Tasks/2.  interacting with web services/Web Applications and Services/Project Problem Statement.py
@@ -0,0 +1,11 @@
+Para completar este módulo, escribirá un script que interactúe con un servicio web en ejecución. El servicio web es parte del sitio web de su empresa y se encarga de almacenar y mostrar las opiniones de los clientes de la empresa.
+
+Las revisiones se almacenan en archivos de texto en el disco local. Su secuencia de comandos debe abrir esos archivos, procesar la información para convertirla en el formato esperado por el servicio web y luego enviarla al servicio web para que se almacene.
+
+Para este laboratorio, el servicio se ejecuta en la misma máquina y, si lo desea, puede ver cómo se implementa todo. Pero no necesita cambiar la implementación del servicio para completar el ejercicio.
+
+Recuerda que puedes tomarte tu tiempo para preparar el código que escribirás. Puede comenzar el laboratorio más adelante, una vez que tenga una buena idea de lo que hará y cómo lo hará.
+
+Además, siéntete libre de revisar los recursos que señalamos tantas veces como necesites.
+
+¡Buena suerte, tienes esto!
